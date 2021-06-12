733dc2109a89c12faf037ddaa04f2d2ef04ee653
KatePodolsky <Doogmatix91@gmail.com>
2021-06-12 13:30:06 +0300

    Add task

--- src/App.js
+++ src/App.js
@@ -1,32 +1,86 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import shortid from 'shortid';
+
 import Container from "./components/Container/Container";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import FilterContacts from "./components/FilterContacts/FilterContacts"
+
+import contactInitial from "./data/contactInitial.json";
+import styles from "./App.module.css"
+
 
 class App extends Component {
- static defaultProps = {
-    contacts: [],
-    name: ''
+  static defaultProps = {
+    contacts: contactInitial,
+    filter: ''
   };
 
-  // static propTypes = {
-  //   good: PropTypes.number,
-  //   neutral: PropTypes.number,
-  //   bad: PropTypes.number,
-  // };
+  static propTypes = {
+    contacts: PropTypes.array,
+    filter: PropTypes.string,
+  };
 
   state = {
     contacts: this.props.contacts,
-    name: this.props.name,
+    filter: this.props.filter,
   };
+
+  formAddContact = ({ name, number }) => {
+    const { contacts } = this.state;
+    const contact = {
+      id: shortid.generate(),
+      name: name,
+      number: number,
+    };
+
+    contacts.some(({ name }) => name.toLowerCase() === contact.name.toLowerCase()) 
+      ? alert(`${name} is already in contacts`)
+      : contacts.some(({ number }) => number === contact.number)
+        ? alert(`${number} is already in contacts under a different name`)
+        : this.setState(({ contacts }) => ({
+      contacts:[contact, ...contacts]
+    }))
   
+  }
+
+  deleteContact = (contactId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({filter:e.currentTarget.value})
+  }
+  
+  getfilteredContacts = () => {
+    const { contacts, filter } = this.state;
+
+    const normalizedFilter = filter.toLowerCase()
+    
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter))
+ }
+
   render() {
+    const { filter } = this.state;
+    const filteredContacts = this.getfilteredContacts();
+
         return (
           <Container>
-            <input
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-              required
+            <h1 className={styles.title}>Phonebook</h1>
+            <ContactForm onSubmit={this.formAddContact} />
+
+            <h2 className={styles.title}>Contacts</h2>
+            <FilterContacts
+              value={filter}
+              onChange={this.changeFilter}
+            />
+            <ContactList
+              contacts={filteredContacts}
+              onDeleteContact={this.deleteContact}
             />
           </Container>
         )
